d6ecc9a4fad754bfccf4d0f9214e42db8a23a9b1
Keeylay <kyrutkowski@gmail.com>
2021-04-26 17:26:28 -0500

    video pre

---
+++ video.js
@@ -0,0 +1,8 @@
+// function play() {
+//     document.querySelector("#video").muted = false;
+//     document.querySelector("#video").play();
+// }
+
+// function pause() {
+//     document.querySelector("#video").pause()
+// }
